c27e90801e7758500d1af3523557114d82cf9991
Yuki Nishijima <ynishijima@pivotal.io>
2016-02-24 10:58:11 +0900

    Rename OsusumeActivityFragment to RetaurantListFragment
    Signed-off-by: Ichizo Umehara <iumehara@pivotal.io>

--- app/src/main/java/io/pivotal/beach/osusume/android/RetaurantListFragment.java
+++ app/src/main/java/io/pivotal/beach/osusume/android/RetaurantListFragment.java
@@ -2,6 +2,7 @@ package io.pivotal.beach.osusume.android;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -26,13 +27,13 @@ import retrofit2.converter.gson.GsonConverterFactory;
 /**
  * A placeholder fragment containing a simple view.
  */
-public class OsusumeActivityFragment extends Fragment {
+public class RetaurantListFragment extends Fragment {
     private RecyclerView recyclerView;
     private RecyclerView.Adapter adapter;
     private RecyclerView.LayoutManager layoutManager;
     private List<Restaurant> restaurantList;
 
-    public OsusumeActivityFragment() {
+    public RetaurantListFragment() {
     }
 
     @Override
@@ -41,7 +42,7 @@ public class OsusumeActivityFragment extends Fragment {
         restaurantList = new ArrayList<Restaurant>();
         adapter = new RestaurantAdapter(restaurantList);
 
-        View rootView = inflater.inflate(R.layout.fragment_osusume, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_restaurant_list, container, false);
 
         recyclerView = (RecyclerView) rootView.findViewById(R.id.restaurantListView);
         recyclerView.setAdapter(adapter);
@@ -91,16 +92,31 @@ public class OsusumeActivityFragment extends Fragment {
             this.restaurants = restaurants;
         }
 
-        public class ViewHolder extends RecyclerView.ViewHolder {
+        class ViewHolder extends RecyclerView.ViewHolder {
             public TextView restaurantNameView;
             public TextView restaurantAuthorView;
             public TextView restaurantCreatedAtView;
 
-            public ViewHolder(View containerView, TextView restaurantCreatedAtView, TextView restaurantAuthorView, TextView restaurantNameView) {
+            public ViewHolder(View containerView, TextView restaurantNameView, TextView restaurantAuthorView, TextView restaurantCreatedAtView) {
                 super(containerView);
-                this.restaurantCreatedAtView = restaurantCreatedAtView;
-                this.restaurantAuthorView = restaurantAuthorView;
                 this.restaurantNameView = restaurantNameView;
+                this.restaurantAuthorView = restaurantAuthorView;
+                this.restaurantCreatedAtView = restaurantCreatedAtView;
+
+                containerView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Restaurant restaurant = restaurants.get(ViewHolder.this.getAdapterPosition());
+                        int id = restaurant.getId();
+
+                        RestaurantDetailFragment detailFragment = RestaurantDetailFragment.newInstance(id);
+                        FragmentManager fragmentManager = RetaurantListFragment.this.getFragmentManager();
+                        fragmentManager.beginTransaction()
+                                .replace(R.id.fragment, detailFragment, RestaurantDetailFragment.TAG)
+                                .addToBackStack(null)
+                                .commit();
+                    }
+                });
             }
         }
 
